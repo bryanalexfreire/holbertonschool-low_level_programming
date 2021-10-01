97f981e605ca7a788fea3c3ee48e879097fd87b9
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-01 16:16:20 -0700

    files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: 0 on success
+ */
+#include <stdio.h>
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+/**
+ * main -Print the alphabet in lowercase, and thn in ...
+ * Return: 0 on success
+ */
+#include <stdio.h>
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	return (0);
+}
