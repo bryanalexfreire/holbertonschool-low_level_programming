c0fc96f30a044646649a74de2807a594e6d2639d
Bryan Freire <bryanalexfreire@gmail.com>
2021-09-30 19:58:40 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-/*
- *  * File: 4-puts.c
- */
-
-#include <stdio.h>
-
 /**
- *  * main - Prints "Programming is like building a multilingual puzzle, followed by a new line. Return: Always 0."
- *      */
+ * main - Prints "Programmin is like ...."
+ * Return: Always 0.
+ */
+#include <stdio.h>
 int main(void)
 {
-		puts("\nProgramming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
