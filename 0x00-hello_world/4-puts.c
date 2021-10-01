abb100c15004461baadcbb63ae6cf8921ff5e424
Bryan Freire <bryanalexfreire@gmail.com>
2021-09-30 18:36:13 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ *  * main - Prints "with proper grammar, but the outcome is a piece of art," followed by a new line.
+ *    *
+ *     * Return: Always 0.
+ *      */
 int main(void)
 {
 		puts("Programming is like building a multilingual puzzle");
