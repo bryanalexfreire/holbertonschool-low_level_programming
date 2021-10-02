912fbca4001ef45484bc2ae2e1eab5a3753814c8
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-01 19:47:44 -0700

    fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 /**
  * main - Prints the alphabet in lowercase
- * Return: 0 on success
+ * Return: 0 on success.
  */
 #include <stdio.h>
 int main(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-	putchar('\n');
 	return (0);
 }
