2717f7cda4ddd3f9c09832c00fe2c9ad50b178ce
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-06 10:20:40 -0700

    fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,22 @@
 /**
- * print_sing - Determines whether an inter is positive, negative or zero.
- * @i: the number to be checked.
+ * print_sign - Determines whether an inter is positive, negative or zero.
+ * @n: the number to be checked.
  * Return: 1 if positive, 0 if zero, -1 if negative.
  */
 #include "main.h"
-int print_sign(int i)
+int print_sign(int n)
 {
-	if (i > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (i == 0)
+	else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
 	}
-	else if (i < 0)
+	else
 	{
 		_putchar ('-');
 		return (-1);
