12ba9a2a485d40f02e77dca0035ae470942bd6e7
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-05 21:11:46 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,18 @@
  * main - Function that prints the alphabet, in lowercase
  * Return: Always 0 (Succes)
  */
-#include <studio.h>
+#include <holberton.h>
 int main(void)
 {
-	int n;
-
-	for (n = 'a'; n <= 'z'; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
