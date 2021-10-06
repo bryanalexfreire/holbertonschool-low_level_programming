ea9a9cca923fd7208d6a17e3e347e3497038c91a
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-05 21:19:59 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/** 
+ * print_alphabet_x10 - Make alplabet x10 times
+ * return: void
+ */
+#omcñide "holberton.h"
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c<= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
