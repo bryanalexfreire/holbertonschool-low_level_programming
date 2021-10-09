693bb0ff80c55ed7c76678ddda1cf5b7ecf988ee
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-08 19:04:47 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times
+ */
+#include "main.h"
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
