7043cd9739551997cd4c4cda6a70f8f9ae12cac6
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-08 18:44:55 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - Checks for a digit 0 - 9
+ * @c: number
+ * Return: Integer
+ */
+#include "main.h"
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
