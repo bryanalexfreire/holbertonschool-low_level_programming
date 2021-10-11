ebe2cec8319b09c7dcf71447fb82e0338e2a5dfc
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-11 15:01:47 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap - function that swaps the values of two integers
+ * @a: is a pointer int type
+ * @b: is a pointer int type
+ * Return: always 0.
+ */
+void swap_int(int *a, int *b);
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
