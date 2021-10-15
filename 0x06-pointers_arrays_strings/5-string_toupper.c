de039f433dd6a859159d790db2983ef88933f7aa
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-14 17:59:46 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - convert to uppercase
+ * Return: void
+ * @s: string
+ */
+#include "main.h"
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = 'A' + (s[i] - 'a');
+		}
+		i++;
+	}
+	return (s);
+}
