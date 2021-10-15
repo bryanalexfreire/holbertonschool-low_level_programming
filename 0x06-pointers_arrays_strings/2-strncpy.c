7e69a53940a2817716afa9fe1e0521e103e5dcff
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-14 17:42:56 -0700

    return (dest);

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - concatenates two strings
+ * @dest: dest string
+ * @src: src string
+ * @n: int n to copy
+ * Return: pointer to resulting string
+ */
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+	int s = 0;
+
+	while (src[s] != 0 && s < n)
+	{
+		dest[s] = src[s];
+		s++;
+	}
+	while (s < n)
+	{
+		dest[s] = 0;
+		s++;
+	}
+	return (dest);
+}
