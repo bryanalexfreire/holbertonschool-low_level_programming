a7a1932f86f155018c938b33883130c8006f5aa6
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-25 10:58:55 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - program that prints its name
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0.
+ */
+#include <stdio.h>
+#include "main.h"
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
