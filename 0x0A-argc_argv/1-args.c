e58ceb81e7963fb3326344cdbddf6026ce7bda60
Bryan Freire <bryanalexfreire@gmail.com>
2021-10-25 14:48:18 -0700

    fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,16 +6,9 @@
  */
 #include <stdio.h>
 #include "main.h"
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			printf("%d\n", i - 1);
-	}
+	if (**argv != '\0')
+		printf("%d\n", argc -1);
 	return (0);
 }
