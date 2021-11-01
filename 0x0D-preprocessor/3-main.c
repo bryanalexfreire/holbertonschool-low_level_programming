df5c485690aae1caa5825a024f7c20c66388eebc
Bryan Freire <bryanalexfreire@gmail.com>
2021-11-01 15:16:11 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,17 @@
+/**
+ * main - function-like macro ABS(x) that computes the absolute
+ * Return: 0.
+ */
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	int j;
+
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	printf("%d, %d\n", i, j);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,15 @@
+/**
+ * main - function-like macro SUM(x, y) that computes the sum..
+ * Return: 0.
+ */
+#include "4-sum.h"
+#include "4-sum.h"
+#include <stdio.h>
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
