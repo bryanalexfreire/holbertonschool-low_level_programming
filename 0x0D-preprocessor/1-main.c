d5a635146009296d6f55ecda72bb8cb748db7387
Bryan Freire <bryanalexfreire@gmail.com>
2021-11-01 14:39:59 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,17 @@
+/**
+ * main - defines a macro named PI as an abbreviation for the...
+ * Return: 0.
+ */
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
