18d367513fa425ae38f3b7a10d07981190033a00
Bryan Freire <bryanalexfreire@gmail.com>
2021-11-02 13:53:53 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+/**
+ * new_dog - function that creates a new dog.
+ * @name: variable for dog name
+ * @age: variable for dog age
+ * @owner: variable for dog owner
+ * Return: 0.
+ */
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
