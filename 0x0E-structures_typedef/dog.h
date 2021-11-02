458d4068a9d74a85e3dd47f0a616d02546b517d9
Bryan Freire <bryanalexfreire@gmail.com>
2021-11-01 21:43:36 -0700

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - name for struct
+ * @name: variable for name
+ * @owner: variable for owner
+ * @age: variable for age
+ */
 struct dog
 {
 	char *name;
